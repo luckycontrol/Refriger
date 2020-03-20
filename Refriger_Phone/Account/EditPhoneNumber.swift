89337e082add593286fcad114c5050bf05a87c76
Cho - chong woon <37057884+luckycontrol@users.noreply.github.com>
2020-03-20 20:14:57 +0900

    refriger

--- Refriger_Phone/Account/EditPhoneNumber.swift
+++ Refriger_Phone/Account/EditPhoneNumber.swift
@@ -14,37 +14,48 @@ struct EditPhoneNumber: View {
     @State var authorizedNumber: String = ""
     
     var body: some View {
-        VStack(spacing: 10) {
-            HStack {
-                ZStack {
-                    Rectangle()
-                        .frame(width: 300, height: 50)
-                        .foregroundColor(.white)
-                        .border(Color.black)
-                    
-                    TextField("'-'을 제외하고 입력해주세요.", text: $phoneNumber)
+        VStack {
+            VStack(spacing: 10) {
+                
+                Text("휴대전화번호 인증").font(.system(size: 24, weight: .bold))
+                Text("인증받으신 전화번호가 저장됩니다.").foregroundColor(.gray).opacity(0.8)
+                    .padding(.bottom, 15)
+                
+                HStack {
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 300, height: 50)
+                            .foregroundColor(.white)
+                            .border(Color.black)
+                        
+                        TextField("'-'을 제외하고 입력해주세요.", text: $phoneNumber)
+                            .padding(.leading, 15)
+                    }
+                    Spacer()
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 70, height: 50)
+                            .foregroundColor(.white)
+                            .border(Color.black)
+                        
+                        Text("인증").fontWeight(.semibold)
+                    }
                 }
                 
                 ZStack {
                     Rectangle()
-                        .frame(width: 70, height: 50)
+                        .frame(height: 50)
                         .foregroundColor(.white)
                         .border(Color.black)
                     
-                    Text("인증번호 요청")
+                    TextField("인증번호를 입력해주세요.", text: $authorizedNumber)
+                        .padding(.leading, 15)
                 }
+                Spacer()
             }
-            
-            ZStack {
-                Rectangle()
-                    .frame(height: 50)
-                    .foregroundColor(.white)
-                    .border(Color.black)
-                
-                TextField("인증번호를 입력해주세요.", text: $authorizedNumber)
-            }
-        }
-        .padding(.horizontal, 15)
+            .padding(.top, 30)
+            .padding(.horizontal, 15)
+        }.background(Color("MartBackground"))
     }
 }
 
