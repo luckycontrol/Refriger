0f633c95224568f2424cef96b9d9c1866f0d3ec1
Cho - chong woon <37057884+luckycontrol@users.noreply.github.com>
2020-04-16 10:14:12 +0900

    웹 서버

--- Refriger_Server/web/jsFolder/getOrderList.js
+++ Refriger_Server/web/jsFolder/getOrderList.js
@@ -8,9 +8,48 @@ var table = document.getElementById('OrderList');
 var db = firebase.firestore();
 var ordersRef = db.collection("Orders")
 
+var names = []
+var dates = []
+var addresses = []
+var foodNames = []
+var foodCounts = []
+var hps = []
+
 db.collection("Orders").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         var name = doc.data()["name"].split("-");
-        window.alert(name[0]);
+        var date = doc.data()["OrderDate"].split("-");
+        var address = doc.data()["Address"].split("-");
+        var foodName = doc.data()["foodNames"].split("-");
+        var foodCount = doc.data()["foodCounts"].split("-");
+        var hp = doc.data()["HP"].split("-");
+
+        for(var i=0; i<name.length; i++) {
+            names.push(name[i]);
+            dates.push(date[i]);
+            addresses.push(address[i]);
+            foodNames.push(foodName[i]);
+            foodCounts.push(foodCount[i]);
+            hps.push(hp[i]);
+        }
     });
+    for(var i=0; i<names.length; i++) {
+        var row = table.insertRow(1);
+        row.insertCell(0).innerHTML = names[i];
+        row.insertCell(1).innerHTML = dates[i];
+        row.insertCell(2).innerHTML = addresses[i];
+        row.insertCell(3).innerHTML = foodNames[i];
+        row.insertCell(4).innerHTML = hps[i];
+        row.insertCell(5).innerHTML = "qr코드";
+
+        var done = document.createElement('button');
+        done.innerHTML = "준비완료";
+        done.setAttribute("class", "Order_ready");
+        row.insertCell(6).appendChild(done);
+
+        var refund = document.createElement('button');
+        refund.innerHTML = "환불";
+        refund.setAttribute("class", "Order_refund");
+        row.insertCell(7).appendChild(refund);
+    }
 });
