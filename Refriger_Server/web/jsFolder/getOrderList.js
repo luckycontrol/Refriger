9ad6a564522e503f546eb5f0bae51187fb2f8069
Cho - chong woon <37057884+luckycontrol@users.noreply.github.com>
2020-06-10 00:17:44 +0900

    Refriger Server

--- Refriger_Server/web/jsFolder/getOrderList.js
+++ Refriger_Server/web/jsFolder/getOrderList.js
@@ -15,57 +15,43 @@ db.collection("Orders").get().then((querySnapshot) => {
     // firestore에서 데이터를 가져온다.
     querySnapshot.forEach((doc) => {
         
-        var name = doc.data()["name"].split("-");
-        var date = doc.data()["OrderDate"].split("-");
-        var address = doc.data()["Address"].split("-");
-        var foodName = doc.data()["foodNames"].split("-");
-        var hp = doc.data()["HP"].split("-");
+        let name = doc.data()["name"].split("-");
 
+        let date = doc.data()["OrderDate"].split("-");
+
+        let address = doc.data()["Address"].split("|");
+
+        let foodName_list = doc.data()["foodNames"].split("-");
+
+        let hp = doc.data()["HP"].split("-");
+        
         for(var i=0; i<name.length; i++) {
-            userAccounts.push(doc.id);
-            names.push(name[i]);
-            dates.push(date[i]);
-            addresses.push(address[i]);
-            foodNames.push(foodName[i].split("|"));
-            hps.push(hp[i]);
+
+            let foodName_div = foodName_list[i].split('|');
+
+            let foodName_str = foodName_div.join('<br>');
+
+            let row = table.insertRow(1);
+            row.insertCell(0).innerHTML = name[i];
+            row.insertCell(1).innerHTML = date[i];
+            row.insertCell(2).innerHTML = address[i];
+            row.insertCell(3).innerHTML = foodName_str;
+            row.insertCell(4).innerHTML = hp[i];
+            row.insertCell(5).innerHTML = '<div id = qr_div' + i + '>';
+
+            makeCode(doc.id, i, "qr_div"+i);
+
+            let done = document.createElement('button');
+            done.innerHTML = "준비완료";
+            //done.setAttribute("id", ready_btn);
+            row.insertCell(6).appendChild(done);
+
+            let refund = document.createElement('button');
+            refund.innerHTML = "환불";
+            //refund.setAttribute("id", refund_btn);
+            row.insertCell(7).appendChild(refund);
         }
     });
-
-    // 테이블 Row 만들기
-    for(var i=0; i<names.length; i++) {
-        let row = table.insertRow(1);
-        let f = foodNames[i][0] + "<br>";
-        let qr_div_id = "qr_div" + i;
-        let ready_btn = "ready_btn_" + i;
-        let refund_btn = "refund_btn_" + i;
-        
-        // 주문리스트 String 생성
-        if(foodNames[i].length > 1) {
-            for(var j=1; j<foodNames[i].length; j++) {
-                f += foodNames[i][j] + "<br>";
-            }
-        }
-
-        row.insertCell(0).innerHTML = names[i];
-        row.insertCell(1).innerHTML = dates[i];
-        row.insertCell(2).innerHTML = addresses[i];
-        row.insertCell(3).innerHTML = f;
-        row.insertCell(4).innerHTML = hps[i];
-        row.insertCell(5).innerHTML = '<div id="'+qr_div_id+'">';
-
-        
-        makeCode(userAccounts[i], i, qr_div_id);
-
-        var done = document.createElement('button');
-        done.innerHTML = "준비완료";
-        done.setAttribute("id", ready_btn);
-        row.insertCell(6).appendChild(done);
-
-        var refund = document.createElement('button');
-        refund.innerHTML = "환불";
-        refund.setAttribute("id", refund_btn);
-        row.insertCell(7).appendChild(refund);
-    }
 });
 
 function makeCode(userAccount, index, qr_div_id) {
